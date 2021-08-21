aef777b1127403b8071a78b9ccb548c310f7cf98
a-chap <89303391+a-chap@users.noreply.github.com>
2021-08-21 14:32:51 +0100

    Now blocks stdin, can control blocksize

--- block.c
+++ block.c
@@ -8,6 +8,7 @@ static char *invoc_name = NULL;
 static char *author = "a-chap";
 
 static struct option options[] = {
+    { "block-size", required_argument, NULL, 'b'},
     { "help", no_argument, NULL, 'h'},
     { "version", no_argument, NULL, 'v'},
     { NULL, 0, NULL, 0 }
@@ -19,9 +20,15 @@ static void print_help();
 int main(int argc, char **argv) {
     invoc_name = argv[0];
 
+    int block_size = 5;
     int c;
-    while ((c = getopt_long(argc, argv, "hv", options, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "b:hv", options, NULL)) != -1) {
         switch(c) {
+            case 'b':
+                block_size = strtol(optarg, NULL, 10);
+                if ( block_size <= 0 )
+                    block_size = 5;
+                break;
             case 'h':
                 print_help();
                 exit(EXIT_SUCCESS);
@@ -35,6 +42,12 @@ int main(int argc, char **argv) {
                 exit(EXIT_FAILURE);
                 break;
         }
+    }
+
+    for (int i = 0; (c = getchar() ) != EOF; i++) {
+        if ( i && i % block_size == 0 )
+            printf(" ");
+        printf("%c", c);
     }
 
     return 0;
@@ -51,7 +64,9 @@ static void print_help() {
     printf("Usage: %s [OPTION]...\n"
            "\n"
            "Groups non-whitespace input characters into blocks.\n"
+           "The default block size is 5.\n"
            "\n"
+           "    -b, --block-size SIZE   set the size of the blocks.\n"
            "    -h, --help      display this help and exit.\n"
            "    -v, --version   display version information and exit.\n",
            invoc_name);
