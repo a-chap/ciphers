db5769ee0154bd500ef3c81b9d874d33284290a4
a-chap <89303391+a-chap@users.noreply.github.com>
2021-11-08 12:09:12 +0000

    Added decimation and linear transformation ciphers. Added more options including atbash.

--- shift.c
+++ shift.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 #include <getopt.h>
 
 static char *prog_name = "shift";
-static char *prog_version = "1.0";
+static char *prog_version = "1.1";
 static char *invoc_name = NULL;
 static char *author = "a-chap";
 
 static struct option options[] = {
+    { "multiplier", required_argument, NULL, 'm'},
     { "shift", required_argument, NULL, 's'},
     { "keyword", required_argument, NULL, 'k'},
     { "alphabet", no_argument, NULL, 'a'},
-    { "backwards_alphabet", no_argument, NULL, 'z'},
+    { "reverse-alphabet", no_argument, NULL, 'z'},
     { "progress", no_argument, NULL, 'p'},
+    { "atbash", no_argument, NULL, 'b'},
+    { "caesar", no_argument, NULL, 'c'},
     { "rot13", no_argument, NULL, 'r'},
     { "decrypt", no_argument, NULL, 'd'},
     { "help", no_argument, NULL, 'h'},
@@ -22,74 +25,104 @@ static struct option options[] = {
     { NULL, 0, NULL, 0 }
 };
 
-static int shift = 3;
+static int multiplier = 1;
 static int *keyword = NULL;
-static int keyword_length = 0;
 static int progress_keyword = 0;
 
 static void print_version();
 static void print_help();
 
+static void set_shift(int shift);
 static int valid_keyword(char *keyword);
 static void encrypt(FILE *fp, int decrypt);
 
-static inline int is_simple_shift_cipher() {
-    return keyword == NULL;
-}
-static inline int is_vigenere_cipher() {
-    return keyword != NULL;
-}
-
 int main(int argc, char **argv) {
     invoc_name = argv[0];
-
-    int c, decrypt = 0, tmp_shift;
-    while ((c = getopt_long(argc, argv, "s:k:azprdhv", options, NULL)) != -1) {
+    int decrypt = 0;
+    int c;
+    while ((c = getopt_long(argc, argv, "m:s:k:azpbcrdhv", options, NULL)) != -1) {
         switch(c) {
+            case 'm':
+                multiplier = atoi(optarg);
+                if ( multiplier <= 2  || multiplier >= 26
+                  || multiplier == 13 || (multiplier & 1) == 0 ) {
+                    fprintf(stderr, "%s: Multiplier needs to be an odd number "
+                                    "between 3 and 25 inclusive (except 13).\n",
+                                    invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
+                    exit(EXIT_FAILURE);
+                }
+                break;
             case 's':
-                tmp_shift = atoi(optarg);
-                if ( tmp_shift >= 0 && tmp_shift <= 26 )
-                    shift = tmp_shift;
+                if ( keyword != NULL ) {
+                    fprintf(stderr, "%s: Only one keyword or shift needed.\n",
+                                    invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
+                    exit(EXIT_FAILURE);
+                }
+                set_shift(atoi(optarg));
+                if ( keyword[0] <= 0 || keyword[0] >= 26 ) {
+                    fprintf(stderr, "%s: The shift needs to be a number between "
+                                    "1 and 25 inclusive.\n", invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
+                    exit(EXIT_FAILURE);
+                }
                 break;
             case 'a': case 'z':
                 if ( keyword != NULL ) {
-                                fprintf(stderr, "%s: Only one keyword needed.\n", invoc_name);
-                                fprintf(stderr, "Try '%s --help' for more information.\n", invoc_name);
-                                exit(EXIT_FAILURE);
-                }
-                keyword = calloc(26, sizeof(int));
-                if ( keyword == NULL ) {
-                    perror("");
+                    fprintf(stderr, "%s: Only one keyword needed.\n", invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
                     exit(EXIT_FAILURE);
                 }
-                keyword_length = 26;
+                keyword = calloc(27, sizeof(int));
+                if ( keyword == NULL ) {
+                    perror(invoc_name);
+                    exit(EXIT_FAILURE);
+                }
                 for (int i = 0; i < 26; i++) {
                     keyword[i] = (c == 'a')? i : 25 - i;
                 }
+                keyword[26] = -1;
                 break;
             case 'k':
                 if ( keyword != NULL ) {
                     fprintf(stderr, "%s: Only one keyword needed.\n", invoc_name);
-                    fprintf(stderr, "Try '%s --help' for more information.\n", invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
                     exit(EXIT_FAILURE);
-                } else if ( ! valid_keyword(optarg) ) {
-                    fprintf(stderr, "%s: Keyword must only contain letters.\n", invoc_name);
-                    fprintf(stderr, "Try '%s --help' for more information.\n", invoc_name);
+                }
+                if ( ! valid_keyword(optarg) ) {
+                    fprintf(stderr, "%s: Keyword must only contain letters.\n",
+                                    invoc_name);
+                    fprintf(stderr, "Try '%s --help' for more information.\n",
+                                    invoc_name);
                     exit(EXIT_FAILURE);
                 }
 
-                keyword_length = strlen(optarg);
-                keyword = calloc(keyword_length, sizeof(int));
+                int length = strlen(optarg);
+                keyword = calloc(length + 1, sizeof(int));
 
-                for (int i = 0; i < keyword_length; i++)
-                    keyword[i] = optarg[i] - 'a';
+                keyword[length] = -1;
+                for (int i = 0; i < length; i++)
+                    keyword[i] = tolower(optarg[i]) - 'a';
 
                 break;
             case 'p':
                 progress_keyword = 1;
                 break;
+            case 'c':
+                set_shift(3);
+                break;
             case 'r':
-                shift = 13;
+                set_shift(13);
+                break;
+            case 'b':
+                multiplier = 25;
+                set_shift(25);
                 break;
             case 'd':
                 decrypt = 1;
@@ -103,11 +136,15 @@ int main(int argc, char **argv) {
                 exit(EXIT_SUCCESS);
                 break;
             default:
-                fprintf(stderr, "Try '%s --help' for more information.\n", invoc_name);
+                fprintf(stderr, "Try '%s --help' for more information.\n",
+                                invoc_name);
                 exit(EXIT_FAILURE);
                 break;
         }
     }
+
+    if ( keyword == NULL )
+        set_shift(0);
 
     if ( optind == argc ) {
         encrypt(stdin, decrypt);
@@ -115,7 +152,7 @@ int main(int argc, char **argv) {
         for (int n = optind; n < argc; n++) {
             FILE *fp = fopen(argv[n], "r");
             if ( fp == NULL ) {
-                perror("Cannot open file");
+                perror(invoc_name);
                 continue;
             }
 
@@ -139,33 +176,54 @@ static int valid_keyword(char *keyword) {
     return 1;
 }
 
+static void set_shift(int shift) {
+    keyword = calloc(2,sizeof(int));
+    if ( keyword == NULL ) {
+        perror(invoc_name);
+        exit(EXIT_FAILURE);
+    }
+    keyword[0] = shift;
+    keyword[1] = -1;
+}
+
 static void encrypt(FILE *fp, int decrypt) {
     int c, i = 0;
     int first_letter;
+
+    if ( decrypt ) {
+        int inverse_multipliers[26] = {
+             [3]  =  9, [5]  = 21, [7]  = 15, [9]  =  3,
+             [11] = 19, [15] =  7, [17] = 23, [19] = 11,
+             [21] =  5, [23] = 17, [25] = 25,
+        };
+        multiplier = inverse_multipliers[multiplier];
+    }
+
     while ( ( c = fgetc(fp) ) != EOF ) {
         if ( isalpha(c) ) {
-            if ( is_vigenere_cipher() ) {
-                shift = keyword[i];
+            /* retain this so can restore case after encryption */
+            first_letter = islower(c)? 'a' : 'A';
+            c -= first_letter;
 
-                if ( progress_keyword ) {
-                    keyword[i]++;
-                    keyword[i] %= 26;
-                }
-
-                i = (i + 1) % keyword_length;
+            if ( decrypt ) {
+                c += 26 - keyword[i];
+                c *= multiplier;
+            } else {
+                c *= multiplier;
+                c += keyword[i];
             }
 
-            /*
-             * Retain first letter so I know whether
-             * the letter should come out as upper
-             * or lower case.
-             */
-            first_letter = islower(c)?'a':'A';
-            c = (c - first_letter + (decrypt?26-shift:shift)) % 26
-                + first_letter;
+            c %= 26;
+            c += first_letter; /* revert to character */
 
-            if ( progress_keyword && is_simple_shift_cipher() )
-                shift = (shift + 1) % 26;
+            if ( progress_keyword ) {
+                keyword[i]++;
+                keyword[i] %= 26;
+            }
+
+            i++;
+            if ( keyword[i] == -1 )
+                i = 0;
         }
 
         printf("%c", c);
@@ -183,9 +241,11 @@ static void print_help() {
     printf("Usage: %s [OPTION]... [FILE]...\n"
            "\n"
            "Encrypts given input or FILEs using a shift cipher.\n"
-           "Defaults to a shift of 3.\n"
+           "By default, won't encrypt input at all.\n"
            "\n"
            "    -s, --shift NUMBER  set how many letters to shift the plain text by.\n"
+           "    -m, --multiplier NUMBER  multiply the value of each letter by NUMBER.\n"
+           "                             The letters have the values a = 0, b = 1, ... z = 25.\n"
            "    -k, --keyword WORD  set the keyword to use a Vigenere cipher.\n"
            "    -a, --alphabet  use the alphabet as the keyword for the Vigenere cipher.\n"
            "    -z, --backwards_alphabet  use the alphabet but backwards as the keyword for the Vigenere cipher.\n"
@@ -195,7 +255,9 @@ static void print_help() {
            "                    and so on. If using a simple shift, it will be\n"
            "                    incremented after being used for a letter and\n"
            "                    will wrap around to 0 when 26.\n"
+           "    -c, --caesar    use a shift of 3 letters.\n"
            "    -r, --rot13     use a shift of 13 letters.\n"
+           "    -b, --atbash    use atbash cipher -- swap a with z, b with y etc.\n"
            "    -d, --decrypt   decrypt cipher text.\n"
            "    -h, --help      display this help and exit.\n"
            "    -v, --version   display version information and exit.\n",
